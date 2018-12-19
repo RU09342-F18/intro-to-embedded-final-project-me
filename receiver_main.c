5fd4b392a4e9f9fbb1e2bd13f403eb43c59a369c
bakerr5 <36169134+bakerr5@users.noreply.github.com>
2018-12-19 15:52:56 -0500

    updated to work with servo rather than LED

--- receiver_main.c
+++ receiver_main.c
@@ -25,13 +25,13 @@ void main(void)
    P1SEL2 |= 0x06;                  // ...by settings both of these registers
 
    // configure timer A1
-   TA1CCTL1 = OUTMOD_3;             // set to OUTMOD_3 for pwm set/reset
-   TA1CCTL2 = OUTMOD_3;             // set to OUTMOD_3 for pwm set/reset
-   TA1CCR0 = 0x1F;                  // set to 0x1F
-   TA1CCR1 = 0x00;                  // set to 0x00
-   TA1CCR2 = 0x00;                  // set to 0x00
-   TA1CTL = TASSEL_2 + MC_1 + ID_2 + TACLR; // set to use SMCLK (TASSEL_2) and count to CCR0 in up
-                                    // mode (MC_1) divided by 4 (ID_2)
+   TA1CCTL1 = OUTMOD_7;             // set to OUTMOD_7 for pwm reset/set
+   TA1CCTL2 = OUTMOD_7;             // set to OUTMOD_7 for pwm reset/set
+   TA1CCR0 = 0x4E20;                // set to 20000 (20ms period)
+   TA1CCR1 = 0x03E8;                // set to 1000 (1ms pulse)
+   TA1CCR2 = 0x07D0;                // set to 2000 (2ms pulse)
+   TA1CTL = TASSEL_2 + MC_1 + TACLR; // set to use SMCLK (TASSEL_2) and count to CCR0 in up
+                                    // mode (MC_1)
 
    // configure UART... (UCA0CTL1) / ((UCA0BR1 x 256) + UCA0BR0 + (UCA0MCTL / 8)) = baud rate
    UCA0CTL1 |= UCSSEL_2;            // set to use SMCLK (UCSSEL_2)
@@ -52,22 +52,22 @@ __interrupt void USCI0RX_ISR(void)
    serialReg = UCA0RXBUF;           // store the value
 
    // BT module outputs 0xFF before connection is established so the most significant bit is not
-   // used in our communication and is checked to be 0 before accepting data
+   // used in the communication and is checked to be 0 before accepting data
    if ((serialReg & 128) == 0)
    {
       // if the potentiometer is left of center
       if (((serialReg & 0x3F) >= 0) && ((serialReg & 0x3F) <= 0x1F))
       {
           // set PWM for servos
-          TA1CCR1 = 0;
-          TA1CCR2 = 0x1F - (serialReg & 0x1F);
+          TA1CCR1 = 0x07D0 - (serialReg & 0x1F)*32; // value between 0x03E8 and 0x07D0;
+          TA1CCR2 = 0x07D0;
       }
       // if the potentiometer is right of center
       else
       {
           // set PWM for servos
-          TA1CCR1 = serialReg & 0x1F;
-          TA1CCR2 = 0;
+          TA1CCR1 = 0x03E8;
+          TA1CCR2 = 0x07D0 - (serialReg & 0x1F)*32; // value between 0x03E8 and 0x07D0;
       }
    }
 }
